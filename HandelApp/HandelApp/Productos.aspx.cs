f62bceef5333b42ebbe45e4f6aeb10f5dbe6eef6
CrisMoreira2510 <106870383+CrisMoreira2510@users.noreply.github.com>
2023-11-12 00:00:55 -0300

    ddl marca y categoria en agregar, para producto.aspx.cs y boton agregar

--- HandelApp/HandelApp/Productos.aspx.cs
+++ HandelApp/HandelApp/Productos.aspx.cs
@@ -18,6 +18,28 @@ namespace HandelApp
             List<Producto> lista = new List<Producto>();
             lista = ProductoNegocio.listarconSp();
 
+            Marca marcaaux = new Marca();
+            MarcaNegocio marNegAux = new MarcaNegocio();
+            List<Marca> listaMarcas = new List<Marca>();
+
+            Categoria categoriaaux = new Categoria();
+            CategoriaNegocio catNegAux = new CategoriaNegocio();
+            List<Categoria> listaCategorias = new List<Categoria>();
+
+            listaMarcas = marNegAux.listar();
+            listaCategorias = catNegAux.listar();
+
+
+            ddlMarca.DataSource = listaMarcas;
+            ddlMarca.DataTextField = "Descripcion";
+            ddlMarca.DataValueField = "ID";
+            ddlMarca.DataBind();
+
+            ddlCategoria.DataSource = listaCategorias;
+            ddlCategoria.DataTextField = "Descripcion";
+            ddlCategoria.DataValueField = "Id";
+            ddlCategoria.DataBind();
+
             dgvProductos.DataSource = lista;
             dgvProductos.DataBind();
 
@@ -25,7 +47,43 @@ namespace HandelApp
 
         protected System.Void btnAgregarProd_Click()
         {
+            ProductoNegocio auxProdNegocio = new ProductoNegocio();
 
+            try
+            {
+                if (producto == null) { producto = new Producto(); }
+                if (producto.Marca == null) { producto.Marca = new Marca(); }
+                if (producto.Categoria == null) { producto.Categoria = new Categoria(); }
+
+
+                producto.Nombre = txtNombre.Text;
+                producto.Descripcion = txtDescripcion.Text;
+                producto.Codigo = txtCodigo.Text;
+                producto.Marca.ID = int.Parse(ddlMarca.SelectedValue);
+                producto.Categoria.Id = int.Parse(ddlCategoria.SelectedValue);
+                producto.StockTotal = int.Parse(txtStockTotal.Text);
+                producto.StockMinimo = int.Parse(txtStockMinimo.Text);
+                producto.PrecioCompra = decimal.Parse(txtPrecio.Text);
+
+                auxProdNegocio.alta(producto);
+                lblMensaje.Text = "Producto cargado con éxito";
+
+
+            }
+            catch (FormatException ex)
+            {
+                lblMensaje.Text = "Ingresa un precio en números por favor";
+            }
+
+            catch (OverflowException ex)
+            {
+                lblMensaje.Text = "Superaste la cantidad de caracteres";
+            }
+
+            catch (Exception ex)
+            {
+                lblMensaje.Text = ex.ToString();
+            }
         }
     }
 }
