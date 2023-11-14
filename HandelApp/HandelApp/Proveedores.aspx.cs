dcc5323f2f6594a22dcd6f371cc3c97d53dde5ae
GeraMontemuro <gmontemuro@hotmail.com>
2023-11-14 20:28:02 -0300

    Add AB Proveedorses
    Realizado

--- HandelApp/HandelApp/Proveedores.aspx.cs
+++ HandelApp/HandelApp/Proveedores.aspx.cs
@@ -26,11 +26,49 @@ namespace HandelApp
 
         protected void dgvProveedores_RowCommand(object sender, GridViewCommandEventArgs e)
         {
+            int id = Convert.ToInt32(e.CommandArgument);
+
+            if(e.CommandName == "Eliminar")
+            {
+
+                ProveedorNegocio ProveedorAEliminar = new ProveedorNegocio();
+                try
+                {
+                    ProveedorAEliminar.baja(id);
+                    Response.Redirect("Proveedores.aspx");
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+
+
+            }
 
         }
 
         protected void btnAgregarProd_Click(object sender, EventArgs e)
         {
+            ProveedorNegocio ProNegocio = new ProveedorNegocio ();
+
+            try
+            {
+                if(proveedor == null) { proveedor = new Proveedor(); }
+
+                proveedor.NombreFantasia = txtNombre.Text;
+                proveedor.Cuil = txtCuil.Text;  
+                proveedor.Telefono= txtTelefono.Text;   
+                proveedor.Mail = txtMail.Text;
+
+                ProNegocio.alta(proveedor);
+                Response.Redirect("Proveedores.aspx");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
 
         }
     }
